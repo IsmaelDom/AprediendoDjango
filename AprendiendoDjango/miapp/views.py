9fe1b85e3bc62a23208ceed904c991442fdb591f
Ismael Dominguez <ismaeldom11@gmail.com>
2021-09-10 13:47:50 -0500

    Se agrega logica en la vista solo para pruebas.

--- AprendiendoDjango/miapp/views.py
+++ AprendiendoDjango/miapp/views.py
@@ -2,9 +2,22 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("""
+    html = """
         <h1>Inicio</h1>
-    """)
+        <p>Años hasta el 2050:</p>
+        <ul>
+    """
+    year = 2021
+    while year <= 2050:
+
+        if year % 2 == 0:
+            html += f"<li>{str(year)}</li>"
+            
+        year += 1
+
+    html += "</ul>"
+
+    return HttpResponse(html)
 
 def hola_mundo(request):
     return HttpResponse("""
