4c5bad14ad883c197dd2bca7c8004a4ccb17ccb0
Ismael Dominguez <ismaeldom11@gmail.com>
2021-09-10 13:21:47 -0500

    Se crea primera vista.

--- AprendiendoDjango/miapp/views.py
+++ AprendiendoDjango/miapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def hola_mundo(request):
+    return HttpResponse("""
+        <h1>Hola Mundo desde Django</h1>
+        <h3>Bienvenido</h3>
+    """)
